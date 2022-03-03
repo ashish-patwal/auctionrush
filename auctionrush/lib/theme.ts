01d4e9b8e5fef3cd052eaf5548bc91ee978a21c2
ashish-patwal <ashishpatwal147@gmail.com>
2022-03-03 18:37:16 +0530

    update 3 Mar 2022

---
+++ auctionrush/lib/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  moe_blue: '#BCF3FC',
+  moe_bluee: '#A0E5FB',
+  moe_blueee: '#5DD4FA',
+  moe_skin: '#FAFAEF',
+  moe_pink: '#FBE3E2',
+  moe_pinkk: '#F6C2E1',
+  moe_red: '#F7A1A8',
+  moe_redd: '#FC8B96',
+  moe_green: '#9AFAC2',
+  moe_greenn: '#82FBC5'
+}
+
+const theme = extendTheme({ colors })
+export default theme
